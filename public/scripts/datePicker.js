c31cc509390d4368df2530d0f54d35c776daafaf
Jonathan Allard <jta.allard@gmail.com>
2025-08-16 14:57:18 -0700

    Create date picker as a more generialized component that can be used in multiple places.

--- public/scripts/datePicker.js
+++ public/scripts/datePicker.js
@@ -1,91 +1,168 @@
 class DatePicker {
-    constructor(element) {
-        this.id = Math.random().toString(36).substring(2, 15);
+    constructor(id, element) {
+        this.id = id;
         this.isOpen = false;
         this.self = element;
     }
 }
-const datePicker = document.getElementById("datePickerBtn");
-const dropdownMenu = document.getElementById("dropdownMenu");
 var datePickerIsOpen = false;
+var datePickerCallback = null;
 var pickers = [];
 window.onload = function () {
-    const elements = document.getElementsByClassName("datePicker");
+    const elements = document.getElementsByClassName("datepicker");
+    // Create a DatePicker instance for each element with the class "datepicker
     for (let i = 0; i < elements.length; i++) {
-        const element = elements[i];
-        pickers.push(new DatePicker(element));
+        const id = Math.random().toString(36).substring(2, 15);
+        elements[i].innerHTML = `
+            <button class="datePickerBtn" onclick="">
+                Date Range:
+                <span class="greyText">00/00/2025 - 00/00/2025</span>
+            </button>
+        `;
+        elements[i].innerHTML += `
+            <div class="datePicker-dropdown">
+                <a href="#" onclick="setDateToday('${id}')">Today</a>
+                <a href="#" onclick="setDateYesterday('${id}')">Yesterday</a>
+                <a href="#" onclick="setDateThisMonth('${id}')">This Month</a>
+                <a href="#" onclick="setDateLastMonth('${id}')">Last Month</a>
+                <a href="#" onclick="setDateLast3Months('${id}')">Last 3 Months</a>
+                <a href="#" onclick="setDateThisYear('${id}')">This Year</a>
+                <a href="#" id="customDateBtn_${id}" onclick="showCustomDatePicker('${id}')">Custom</a>
+            </div> 
+        `;
+        // On click, toggle the dropdown menu position
+        const dropdownMenu = elements[i].querySelector(".datePicker-dropdown");
+        elements[i].addEventListener("click", function () {
+            console.log("Date picker clicked");
+            const rect = elements[i].getBoundingClientRect();
+            const margin = 10;
+            dropdownMenu.style.top = `${rect.bottom + window.scrollY + margin}px`;
+            dropdownMenu.style.left = `${rect.left}px`;
+            dropdownMenu.classList.add("show");
+        });
+        // Initialize the date range picker
+        $(`#customDateBtn_${id}`).daterangepicker({
+            alwaysShowCalendars: true,
+        }, function (start, end, label) {
+            const startDate = start.format('MM/DD/YYYY');
+            const endDate = end.format('MM/DD/YYYY');
+            const picker = pickers.find(p => p.id === id);
+            picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startDate} - ${endDate}</span>`;
+            if (datePickerCallback) {
+                datePickerCallback(startDate, endDate);
+            }
+            datePickerIsOpen = false;
+        });
+        pickers.push(new DatePicker(id, elements[i]));
     }
-    console.log("Date pickers initialized:", pickers);
 };
-datePicker.addEventListener("click", function () {
-    console.log("Date picker clicked");
-    const rect = datePicker.getBoundingClientRect();
-    const margin = 10;
-    dropdownMenu.style.top = `${rect.bottom + window.scrollY + margin}px`;
-    dropdownMenu.style.left = `${rect.left}px`;
-    dropdownMenu.classList.toggle("show");
-});
 // Close dropdown if clicked outside
 window.addEventListener("click", (event) => {
     const targetElement = event.target;
-    if (!targetElement.matches(".datePickerBtn") && !datePickerIsOpen) {
+    if (!targetElement.matches(".datePickerBtn") &&
+        !targetElement.closest(".datePicker-dropdown") &&
+        !datePickerIsOpen) {
         console.log("Clicked outside the date picker, closing dropdown");
-        dropdownMenu.classList.remove("show");
+        for (let i = 0; i < pickers.length; i++) {
+            const picker = pickers[i];
+            const dropdown = picker.self.querySelector(".datePicker-dropdown");
+            if (dropdown.classList.contains("show")) {
+                dropdown.classList.remove("show");
+            }
+        }
     }
 });
-$('#customDateBtn').daterangepicker({}, function (start, end, label) {
-    const startDate = start.format('MM/DD/YYYY');
-    const endDate = end.format('MM/DD/YYYY');
-    document.getElementById("datePicker").innerHTML = `Date Range: <span class="greyText">${startDate} - ${endDate}</span>`;
-    console.log(`Selected date range: ${startDate} - ${endDate}`);
-    datePickerIsOpen = false;
-});
-$('#customDateBtn').on('hide.daterangepicker', function (event) {
-    event.stopPropagation(); // Prevent the click from propagating to the document
-    datePickerIsOpen = false; // Set the flag to true when the custom date button is clicked
-});
 function showCustomDatePicker(datePickerId) {
-    $(`#${datePickerId}`).show();
+    console.log(`Showing custom date picker for ${datePickerId}`);
+    $(`#${datePickerId}`).children(".datePicker-dropdown").addClass("show");
     datePickerIsOpen = true;
 }
 function setDateToday(datePickerId) {
     const today = moment().format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${today} - ${today}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${today} - ${today}</span>`;
+    }
+    const dropdown = picker.self.querySelector(".datePicker-dropdown");
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+    datePickerCallback(today, today);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 function setDateYesterday(datePickerId) {
     const yesterday = moment().subtract(1, 'days').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${yesterday} - ${yesterday}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${yesterday} - ${yesterday}</span>`;
+    }
+    const dropdown = picker.self.querySelector(".datePicker-dropdown");
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+    datePickerCallback(yesterday, yesterday);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 function setDateThisMonth(datePickerId) {
     const startOfMonth = moment().startOf('month').format('MM/DD/YYYY');
     const endOfMonth = moment().endOf('month').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${startOfMonth} - ${endOfMonth}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startOfMonth} - ${endOfMonth}</span>`;
+    }
+    const dropdown = picker.self.querySelector(".datePicker-dropdown");
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+    datePickerCallback(startOfMonth, endOfMonth);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 function setDateLastMonth(datePickerId) {
     const startOfLastMonth = moment().subtract(1, 'month').startOf('month').format('MM/DD/YYYY');
     const endOfLastMonth = moment().subtract(1, 'month').endOf('month').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${startOfLastMonth} - ${endOfLastMonth}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startOfLastMonth} - ${endOfLastMonth}</span>`;
+    }
+    const dropdown = picker.self.querySelector(".datePicker-dropdown");
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+    datePickerCallback(startOfLastMonth, endOfLastMonth);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 function setDateLast3Months(datePickerId) {
     const startOfLast3Months = moment().subtract(3, 'months').startOf('month').format('MM/DD/YYYY');
     const endOfLast3Months = moment().endOf('month').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${startOfLast3Months} - ${endOfLast3Months}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startOfLast3Months} - ${endOfLast3Months}</span>`;
+    }
+    const dropdown = picker.self.querySelector(".datePicker-dropdown");
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+    datePickerCallback(startOfLast3Months, endOfLast3Months);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 function setDateThisYear(datePickerId) {
     const startOfYear = moment().startOf('year').format('MM/DD/YYYY');
     const endOfYear = moment().endOf('year').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${startOfYear} - ${endOfYear}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startOfYear} - ${endOfYear}</span>`;
+    }
+    const dropdown = picker.self.querySelector(".datePicker-dropdown");
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+    datePickerCallback(startOfYear, endOfYear);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 //# sourceMappingURL=datePicker.js.map
--- public/scripts/datePicker.ts
+++ public/scripts/datePicker.ts
@@ -4,115 +4,216 @@ class DatePicker
     isOpen: boolean;
     self: HTMLElement;
 
-    constructor(element: HTMLElement) {
-        this.id = Math.random().toString(36).substring(2, 15);
+    constructor(id: string, element: HTMLElement) {
+        this.id = id;
         this.isOpen = false;
         this.self = element
     }
 }
 
-const datePicker = document.getElementById("datePickerBtn");
-const dropdownMenu = document.getElementById("dropdownMenu");
-var datePickerIsOpen = false;
-
-var pickers: DatePicker[] = [];
+var datePickerIsOpen             = false;
+var datePickerCallback: Function = null;
+var pickers: DatePicker[]        = [];
 
 window.onload = function() 
 {
-    const elements = document.getElementsByClassName("datePicker");
+    const elements = document.getElementsByClassName("datepicker");
 
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements[i] as HTMLElement;
-        pickers.push(new DatePicker(element));
+    // Create a DatePicker instance for each element with the class "datepicker
+    for (let i = 0; i < elements.length; i++) 
+    {
+        const id = Math.random().toString(36).substring(2, 15);
+        elements[i].innerHTML = `
+            <button class="datePickerBtn" onclick="">
+                Date Range:
+                <span class="greyText">00/00/2025 - 00/00/2025</span>
+            </button>
+        `;
+
+        elements[i].innerHTML += `
+            <div class="datePicker-dropdown">
+                <a href="#" onclick="setDateToday('${id}')">Today</a>
+                <a href="#" onclick="setDateYesterday('${id}')">Yesterday</a>
+                <a href="#" onclick="setDateThisMonth('${id}')">This Month</a>
+                <a href="#" onclick="setDateLastMonth('${id}')">Last Month</a>
+                <a href="#" onclick="setDateLast3Months('${id}')">Last 3 Months</a>
+                <a href="#" onclick="setDateThisYear('${id}')">This Year</a>
+                <a href="#" id="customDateBtn_${id}" onclick="showCustomDatePicker('${id}')">Custom</a>
+            </div> 
+        `;
+
+        // On click, toggle the dropdown menu position
+        const dropdownMenu = elements[i].querySelector(".datePicker-dropdown") as HTMLElement;
+        elements[i].addEventListener("click", function() 
+        {
+            console.log("Date picker clicked");
+            
+            const rect = elements[i].getBoundingClientRect();
+            const margin = 10; 
+
+            dropdownMenu.style.top = `${rect.bottom + window.scrollY + margin}px`;
+            dropdownMenu.style.left = `${rect.left}px`;
+            dropdownMenu.classList.add("show");
+        }); 
+
+        // Initialize the date range picker
+        $(`#customDateBtn_${id}`).daterangepicker({
+            alwaysShowCalendars: true,
+        }, function(start, end, label) 
+        {
+            const startDate = start.format('MM/DD/YYYY');
+            const endDate = end.format('MM/DD/YYYY');
+            const picker = pickers.find(p => p.id === id);
+            picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startDate} - ${endDate}</span>`;
+
+            if (datePickerCallback) {
+                datePickerCallback(startDate, endDate);
+            }
+
+            datePickerIsOpen = false;
+        });
+
+        pickers.push(new DatePicker(id, elements[i] as HTMLElement));
     }
-
-    console.log("Date pickers initialized:", pickers);
 }
-
-datePicker.addEventListener("click", function() 
-{
-    console.log("Date picker clicked");
-    
-    const rect = datePicker.getBoundingClientRect();
-    const margin = 10; 
-
-    dropdownMenu.style.top = `${rect.bottom + window.scrollY + margin}px`;
-    dropdownMenu.style.left = `${rect.left}px`;
-    dropdownMenu.classList.toggle("show");
-}); 
 
 // Close dropdown if clicked outside
 window.addEventListener("click", (event) => 
 {
     const targetElement = event.target as HTMLElement;
-    if (!targetElement.matches(".datePickerBtn") && !datePickerIsOpen) {
+    if (
+        !targetElement.matches(".datePickerBtn") &&
+        !targetElement.closest(".datePicker-dropdown") &&
+        !datePickerIsOpen) 
+    {
         console.log("Clicked outside the date picker, closing dropdown" );
         
-        dropdownMenu.classList.remove("show");
+        for (let i = 0; i < pickers.length; i++)
+        {
+            const picker = pickers[i];  
+            const dropdown = picker.self.querySelector(".datePicker-dropdown") as HTMLElement;
+            if (dropdown.classList.contains("show")) {
+                dropdown.classList.remove("show");
+            }
+        }
     }
 });
 
-$('#customDateBtn').daterangepicker({}, function(start, end, label) 
+function showCustomDatePicker(datePickerId) 
 {
-    const startDate = start.format('MM/DD/YYYY');
-    const endDate = end.format('MM/DD/YYYY');
-    document.getElementById("datePicker").innerHTML = `Date Range: <span class="greyText">${startDate} - ${endDate}</span>`;
-    console.log(`Selected date range: ${startDate} - ${endDate}`);
-    datePickerIsOpen = false;   
-});
-
-$('#customDateBtn').on('hide.daterangepicker', function(event) {
-    event.stopPropagation(); // Prevent the click from propagating to the document
-    datePickerIsOpen = false; // Set the flag to true when the custom date button is clicked
-});
-
-function showCustomDatePicker(datePickerId) {
-    $(`#${datePickerId}`).show();
+    console.log(`Showing custom date picker for ${datePickerId}`);
+    $(`#${datePickerId}`).children(".datePicker-dropdown").addClass("show");
     datePickerIsOpen = true;
 }
 
-function setDateToday(datePickerId) {
+function setDateToday(datePickerId) 
+{
     const today = moment().format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${today} - ${today}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${today} - ${today}</span>`;
+    }
+
+    const dropdown = picker.self.querySelector(".datePicker-dropdown") as HTMLElement;
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+
+    datePickerCallback(today, today);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 
-function setDateYesterday(datePickerId) {
+function setDateYesterday(datePickerId) 
+{
     const yesterday = moment().subtract(1, 'days').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${yesterday} - ${yesterday}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${yesterday} - ${yesterday}</span>`;
+    }
+
+    const dropdown = picker.self.querySelector(".datePicker-dropdown") as HTMLElement;
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+
+    datePickerCallback(yesterday, yesterday);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 
-function setDateThisMonth(datePickerId) {
+function setDateThisMonth(datePickerId) 
+{
     const startOfMonth = moment().startOf('month').format('MM/DD/YYYY');
     const endOfMonth = moment().endOf('month').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${startOfMonth} - ${endOfMonth}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startOfMonth} - ${endOfMonth}</span>`;
+    }
+
+    const dropdown = picker.self.querySelector(".datePicker-dropdown") as HTMLElement;
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+
+    datePickerCallback(startOfMonth, endOfMonth);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }       
 
-function setDateLastMonth(datePickerId) {
+function setDateLastMonth(datePickerId) 
+{
     const startOfLastMonth = moment().subtract(1, 'month').startOf('month').format('MM/DD/YYYY');
     const endOfLastMonth = moment().subtract(1, 'month').endOf('month').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${startOfLastMonth} - ${endOfLastMonth}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startOfLastMonth} - ${endOfLastMonth}</span>`;
+    }
+
+    const dropdown = picker.self.querySelector(".datePicker-dropdown") as HTMLElement;
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+
+    datePickerCallback(startOfLastMonth, endOfLastMonth);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 
-function setDateLast3Months(datePickerId) {
+function setDateLast3Months(datePickerId) 
+{
     const startOfLast3Months = moment().subtract(3, 'months').startOf('month').format('MM/DD/YYYY');
     const endOfLast3Months = moment().endOf('month').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${startOfLast3Months} - ${endOfLast3Months}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startOfLast3Months} - ${endOfLast3Months}</span>`;
+    }
+
+    const dropdown = picker.self.querySelector(".datePicker-dropdown") as HTMLElement;
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+
+    datePickerCallback(startOfLast3Months, endOfLast3Months);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
 
-function setDateThisYear(datePickerId) {
+function setDateThisYear(datePickerId) 
+{
     const startOfYear = moment().startOf('year').format('MM/DD/YYYY');
     const endOfYear = moment().endOf('year').format('MM/DD/YYYY');
-    document.getElementById(datePickerId).innerHTML = `Date Range: <span class="greyText">${startOfYear} - ${endOfYear}</span>`;
-    dropdownMenu.classList.remove("show");
-    datePickerIsOpen = false;
+    const picker = pickers.find(p => p.id === datePickerId);
+    if (picker) {
+        picker.self.querySelector(".datePickerBtn").innerHTML = `Date Range: <span class="greyText">${startOfYear} - ${endOfYear}</span>`;
+    }
+
+    const dropdown = picker.self.querySelector(".datePicker-dropdown") as HTMLElement;
+    if (dropdown.classList.contains("show")) {
+        console.log("Hiding dropdown menu for today selection");
+        dropdown.classList.remove("show");
+    }
+
+    datePickerCallback(startOfYear, endOfYear);
+    event.stopPropagation(); // Prevent the click event from propagating to the window
 }
